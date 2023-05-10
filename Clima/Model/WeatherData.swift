9685de90b10c5c118b497a08e3e10d1017123d49
Aigera <aikoaliask@gmail.com>
2023-05-10 18:40:18 +0600

    first commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Aika Ali on 07.05.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Aika Ali on 08.05.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
